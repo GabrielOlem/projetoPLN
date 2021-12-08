00ed6f48e0ce25e1952018fb4cd309fbe7557478
GabrielOlem <gme@cin.ufpe.br>
2021-12-08 18:26:48 -0300

    forms continue to story, faq started

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -17,7 +17,41 @@ import json
 
 apiKey = 'ARU5uGFL5Uq1OXr2lHP0QyujAStUvfIU'
 apiSecret = 'KOZpmXWD7OOMwmgx'
-
+respostas = {
+    'meal':
+    {'atlanta+washington': ['fish', 'beef', 'vegetarian'], 
+    'delta': ['vegan', 'fish', 'chicken'],
+    'boston+denver': ['bacon', 'lobster', 'noodles'],
+    'american_airlines': ['tuna sandwich', 'nuts', 'juice', 'wine', 'lasagna', 'sandwich']
+    },
+    'restriction':
+    {'airline':'aiai', 'fromloctoloc':'aiai2'
+    },
+    'capacity':
+    {'airline':'aiai', 'fromloctoloc':'aiai2'
+    },
+    'quantity':
+    {'airline':'aiai', 'fromloctoloc':'aiai2'
+    },
+    'ground_fare':
+    {'airline':'aiai', 'fromloctoloc':'aiai2'
+    },
+    'distance':
+    {'airline':'aiai', 'fromloctoloc':'aiai2'
+    },
+    'airline':
+    {'airline':'aiai', 'fromloctoloc':'aiai2'
+    },
+    'airport':
+    {'airline':'aiai', 'fromloctoloc':'aiai2'
+    },
+    'ground_service':
+    {'airline':'aiai', 'fromloctoloc':'aiai2'
+    },
+    'aircraft':
+    {'airline':'aiai', 'fromloctoloc':'aiai2'
+    },
+}
 class ActionAPITest(Action):
 
     def name(self) -> Text:
@@ -75,3 +109,50 @@ class ActionAPITest(Action):
         dispatcher.utter_message(text='You can go for '+str(dicti2['data'][0]['price']['total']))
 
         return []
+
+
+class ActionQA(Action):
+
+    def name(self) -> Text:
+        return "action_qa"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        if tracker.get_intent_of_latest_message() == "meal":
+            fromloc = tracker.get_slot('fromloc.city_name')
+            toloc = tracker.get_slot('toloc.city_name')
+            airline = tracker.get_slot('airline_name')
+            if airline == None:
+                dispatcher.utter_message(text=f'Vai tomar no cu porra between {fromloc} and {toloc}')
+            else:
+                dispatcher.utter_message(text=f'porra {airline}')
+        elif tracker.get_intent_of_latest_message() == "restriction":
+            fromloc = tracker.get_slot('fromloc.city_name')
+            toloc = tracker.get_slot('toloc.city_name')
+        elif tracker.get_intent_of_latest_message() == "capacity":
+            airline = tracker.get_slot('airline_name')
+            aircraft_code = tracker.get_slot('aircraft_code')
+        elif tracker.get_intent_of_latest_message() == "quantity":
+            fromloc = tracker.get_slot('fromloc.city_name')
+            toloc = tracker.get_slot('toloc.city_name')
+        elif tracker.get_intent_of_latest_message() == "ground_fare":
+            city = tracker.get_slot('city_name')
+            airportName = tracker.get_slot('fromloc.airport_name')
+        elif tracker.get_intent_of_latest_message() == "distance":
+            fromloc = tracker.get_slot('fromloc.city_name')
+            toloc = tracker.get_slot('toloc.city_name')
+        elif tracker.get_intent_of_latest_message() == "airline":
+            fromloc = tracker.get_slot('fromloc.city_name')
+            toloc = tracker.get_slot('toloc.city_name')
+        elif tracker.get_intent_of_latest_message() == "airport":
+            city = tracker.get_slot('city_name')
+        elif tracker.get_intent_of_latest_message() == "ground_service":
+            city = tracker.get_slot('city_name')
+            fromloc = tracker.get_slot('fromloc.city_name')
+        elif tracker.get_intent_of_latest_message() == "aircraft":
+            fromloc = tracker.get_slot('fromloc.city_name')
+            toloc = tracker.get_slot('toloc.city_name')
+        else:
+            dispatcher.utter_message(text='não entrei')
