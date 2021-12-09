cf8627989c8eb949d1edd2e34152ab9bb905ff9f
GabrielOlem <gme@cin.ufpe.br>
2021-12-08 21:51:34 -0300

    api answers provided

--- action_QnA.py
+++ action_QnA.py
@@ -1,144 +1,3 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
-from typing import Any, Text, Dict, List
-
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
-
-import requests
-import json
-
-apiKey = 'ARU5uGFL5Uq1OXr2lHP0QyujAStUvfIU'
-apiSecret = 'KOZpmXWD7OOMwmgx'
-resposta = {
-    'meal':
-    {'atlanta+washington': ['fish', 'beef', 'vegetarian'], 
-    'delta': ['vegan', 'fish', 'chicken'],
-    'boston+denver': ['bacon', 'lobster', 'noodles'],
-    'american_airlines': ['tuna sandwich', 'nuts', 'juice', 'wine', 'lasagna', 'sandwich']
-    },
-    'restriction':
-    {'pittsburgh+atlanta':['mask','vaccination certificate','negative Covid 19 test'],
-    'boston+oakland':['mask','negative Covid 19 test'],
-    'atlanta+washington':['mask','vaccination certificate'],
-    'boston+denver':['mask','negative Covid 19 test']
-    },
-    'capacity':
-    {'100':['256','512'],
-    '72s':['1024','2048'],
-    'american_airlines':['320','640','960'],
-    'united_airlines':['120','240','500','720'] 
-    },
-    'quantity':
-    {'boston+atlanta':['15','3'], #com paradas, direto
-    'san_francisco+denver':['10','5'],
-    'atlanta+washington':['3','1'],
-    'boston+denver':['15','0']
-    },
-    'ground_fare':
-    {'pittsburgh':['300','40'],                 #limo, carro normal
-    'tacoma':['450','35'],
-    'atlanta_airport':['399','50'],
-    'philadelphia_international_airport':['500','100']
-    },
-    'distance':
-    {'los_angeles+boston':['2590'],               # in miles
-    'pittsburgh+san_francisco':['2260'],
-    'boston+denver':['1765'],
-    'atlanta+washington':['542']
-    },
-    'airline':
-    {'los_angeles+boston':['American airlines','United airlines','Delta'],               # in miles
-    'pittsburgh+san_francisco':['Delta','United airlines'],
-    'boston+denver':['American airlines'],
-    'atlanta+washington':['Delta']
-    },
-    'airport':
-    {
-    'orlando':['Orlando International Airport'],
-    'new_york':['New York JFK International Airport','New York La Guardia Airport','Syracuse - Hancock Intl. Airport'],
-    'denver':['Denver International Airport','Rocky Mountain Metropolitan Airport','Boulder Municipal Airport'],
-    'boston':['Worcester Regional Airport','Logan International Airport','Manchester-Boston Regional Airport','T. F. Green Airport']
-    },
-    'ground_service':
-    {'denver':['Uber','taxi','bus'],
-    'atlanta':['Uber','bus'],
-    'baltimore':['Uber','taxi'],
-    'san_diego_airport':['bus']
-    },
-    'aircraft':
-    {'cleveland+dallas':['Turboprop Aircraft','Piston Aircraft','Jets','Mid-Size Jets'],
-    'pittsburgh+baltimore':['Mid-Size Jets','Regional Jets','Narrow Body Aircraft'],
-    'atlanta+boston':['Wide Body Airliners','Commuter liners'],
-    'boston+san_francisco':['Airbus','Concorde','Military Aircraft','Multi-role Combat']
-    },
-}
-class ActionAPITest(Action):
-
-    def name(self) -> Text:
-        return "action_api_test"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        headers = {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        }
-
-        data = {
-            'grant_type': 'client_credentials',
-            'client_id': apiKey,
-            'client_secret': apiSecret
-        }
-
-        response = requests.post('https://test.api.amadeus.com/v1/security/oauth2/token', headers=headers, data=data)
-        dicti = json.loads(response.text)
-
-        headers = {
-            'Authorization': 'Bearer ' + dicti['access_token'],
-        }
-
-        params = (
-            ('subType', 'CITY'),
-            ('keyword', str(tracker.get_slot('fromloc.city_name'))),
-        )
-        response2 = requests.get('https://test.api.amadeus.com/v1/reference-data/locations', headers=headers, params=params)
-
-        fromCity = json.loads(response2.text)['data'][0]['iataCode']
-
-        params = (
-            ('subType', 'CITY'),
-            ('keyword', str(tracker.get_slot('toloc.city_name'))),
-        )
-
-        response2 = requests.get('https://test.api.amadeus.com/v1/reference-data/locations', headers=headers, params=params)
-
-        toCity = json.loads(response2.text)['data'][0]['iataCode']
-
-        params = (
-            ('originLocationCode', str(fromCity)),
-            ('destinationLocationCode', str(toCity)),
-            ('departureDate', '2021-12-15'),
-            ('adults', 1),
-        )
-
-        response2 = requests.get('https://test.api.amadeus.com/v2/shopping/flight-offers', headers=headers, params=params)
-
-        dicti2 = json.loads(response2.text)
-
-        dispatcher.utter_message(text='You can go for '+str(dicti2['data'][0]['price']['total']))
-
-        return []
-
-
 class ActionQA(Action):
 
     def name(self) -> Text:
@@ -149,7 +8,6 @@ class ActionQA(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
         intent = tracker.get_intent_of_latest_message()
-        print(intent)
         if intent == "meal":
             fromloc = tracker.get_slot('fromloc.city_name')
             toloc = tracker.get_slot('toloc.city_name')
@@ -160,7 +18,7 @@ class ActionQA(Action):
             if airline != None:
                 meals =''
                 # we have the options {meal} for ...
-                if airline.replace(' ', '_') in resposta[intent].keys():
+                if airline in resposta[intent].keys():
                     meals_vec = resposta[intent][airline]
                     if len(meals_vec) == 1: 
                         message = f"We have the option of {meals_vec[0]} for the airline {airline.upper()}"
@@ -174,7 +32,7 @@ class ActionQA(Action):
             else:
                 meals =''
                 # we have the options {meal} for ...
-                key = f"{fromloc}+{toloc}".replace(' ', '_')
+                key = f"{fromloc}+{toloc}"
 
                 if key in resposta[intent].keys():
                     meals_vec = resposta[intent][key]
@@ -195,7 +53,7 @@ class ActionQA(Action):
             toloc = tracker.get_slot('toloc.city_name')
 
             message = ''
-            key = f"{fromloc}+{toloc}".replace(' ', '_')
+            key = f"{fromloc}+{toloc}"
 
             if key in resposta[intent].keys():
                 items = ''
@@ -204,7 +62,7 @@ class ActionQA(Action):
                     message = f"The costumer have the obrigation to have {vec[0]} for the flight between {fromloc.upper()} and {toloc.upper()}"
                 else:
                     for item in vec[:-1]:
-                        items = items + f" {item},"
+                        items = items + f" {meal},"
                     items = items[:-1] + f" and {vec[-1]}"
                     message = f"The costumer have the obrigation to have {items} for the flight between {fromloc.upper()} and {toloc.upper()}"
             else:
@@ -221,7 +79,7 @@ class ActionQA(Action):
             message = ''
 
             if airline != None:
-                key = airline.replace(' ', '_')
+                key = airline
 
                 if key in resposta[intent].keys():
                     items = ''
@@ -230,14 +88,14 @@ class ActionQA(Action):
                         message = f"The airline {airline.upper()} has airplanes with the capacity of {vec[0]}"
                     else:
                         for item in vec[:-1]:
-                            items = items + f" {item},"
+                            items = items + f" {meal},"
                         items = items[:-1] + f" and {vec[-1]}"
                         message = f"The airline {airline.upper()} has airplanes with the capacity of {items}"
                 else:
                     message = f"Sorry we dont have informations about capacity for the airline {airline.upper()}"
 
             else:
-                key = aircraft_code.replace(' ', '_')
+                key = aircraft_code
 
                 if key in resposta[intent].keys():
                     items = ''
@@ -246,7 +104,7 @@ class ActionQA(Action):
                         message = f"The aircraft {aircraft_code} has the capacity of {vec[0]}"
                     else:
                         for item in vec[:-1]:
-                            items = items + f" {item},"
+                            items = items + f" {meal},"
                         items = items[:-1] + f" and {vec[-1]}"
                         message = f"The aircraft {aircraft_code} has the capacity of {items}"
                 else:
@@ -260,7 +118,7 @@ class ActionQA(Action):
             toloc = tracker.get_slot('toloc.city_name')
 
             message = ''
-            key = f"{fromloc}+{toloc}".replace(' ', '_')
+            key = f"{fromloc}+{toloc}"
 
             if key in resposta[intent].keys():
                 vec = resposta[intent][key]
@@ -278,7 +136,7 @@ class ActionQA(Action):
             message = ""
 
             if city != None:
-                key = city.replace(' ', '_')
+                key = city
                 if key in resposta[intent].keys():
                     vec = resposta[intent][key]
                     message = f"The ground fares are {vec[0]}$ for a Limusine and {vec[1]} for a normal car at {city.upper()}"
@@ -286,7 +144,7 @@ class ActionQA(Action):
                     message = f"Sorry we dont have informations about the ground fares at {city.upper()}"
 
             else:
-                key = airportName.replace(' ', '_')
+                key = airportName
                 if key in resposta[intent].keys():
                     vec = resposta[intent][key]
                     message = f"The ground fares are {vec[0]}$ for a Limusine and {vec[1]} for a normal car at {airportName.upper()}"
@@ -299,7 +157,7 @@ class ActionQA(Action):
             fromloc = tracker.get_slot('fromloc.city_name')
             toloc = tracker.get_slot('toloc.city_name')
             message = ''
-            key = f"{fromloc}+{toloc}".replace(' ', '_')
+            key = f"{fromloc}+{toloc}"
 
             if key in resposta[intent].keys():
                 vec = resposta[intent][key]
@@ -315,7 +173,7 @@ class ActionQA(Action):
             toloc = tracker.get_slot('toloc.city_name')
 
             message = ''
-            key = f"{fromloc}+{toloc}".replace(' ', '_')
+            key = f"{fromloc}+{toloc}"
 
             if key in resposta[intent].keys():
                 items = ''
@@ -324,7 +182,7 @@ class ActionQA(Action):
                     message = f"We have the airline {vec[0]} for the flight between {fromloc.upper()} and {toloc.upper()}"
                 else:
                     for item in vec[:-1]:
-                        items = items + f" {item},"
+                        items = items + f" {meal},"
                     items = items[:-1] + f" and {vec[-1]}"
                     message = f"We have the airlines {items} for the flights between {fromloc.upper()} and {toloc.upper()}"
             else:
@@ -338,7 +196,7 @@ class ActionQA(Action):
             city = tracker.get_slot('city_name')
 
             message = ''
-            key = city.replace(' ', '_')
+            key = city
 
             if key in resposta[intent].keys():
                 items = ''
@@ -347,7 +205,7 @@ class ActionQA(Action):
                     message = f"We have the airport {vec[0]} at {city.upper()}"
                 else:
                     for item in vec[:-1]:
-                        items = items + f" {item},"
+                        items = items + f" {meal},"
                     items = items[:-1] + f" and {vec[-1]}"
                     message = f"We have the airport {items} at {city.upper()}"
             else:
@@ -363,7 +221,7 @@ class ActionQA(Action):
             message = ""
 
             if city != None:
-                key = city.replace(' ', '_')
+                key = city
                 if key in resposta[intent].keys():
                     items = ''
                     vec = resposta[intent][key]
@@ -371,14 +229,14 @@ class ActionQA(Action):
                         message = f"We have the ground service {vec[0]} at {city.upper()}"
                     else:
                         for item in vec[:-1]:
-                            items = items + f" {item},"
+                            items = items + f" {meal},"
                         items = items[:-1] + f" and {vec[-1]}"
                         message = f"We have the ground services {items} at {city.upper()}"
                 else:
                     message = f"Sorry we dont have informations about the ground services at {city.upper()}"
 
             else:
-                key = fromloc.replace(' ', '_')
+                key = fromloc
                 if key in resposta[intent].keys():
                     items = ''
                     vec = resposta[intent][key]
@@ -386,7 +244,7 @@ class ActionQA(Action):
                         message = f"We have the ground service {vec[0]} at {fromloc.upper()}"
                     else:
                         for item in vec[:-1]:
-                            items = items + f" {item},"
+                            items = items + f" {meal},"
                         items = items[:-1] + f" and {vec[-1]}"
                         message = f"We have the ground service {vec[0]} at {fromloc.upper()}"
                 else:
@@ -400,7 +258,7 @@ class ActionQA(Action):
             toloc = tracker.get_slot('toloc.city_name')
 
             message = ''
-            key = f"{fromloc}+{toloc}".replace(' ', '_')
+            key = f"{fromloc}+{toloc}"
 
             if key in resposta[intent].keys():
                 items = ''
@@ -409,7 +267,7 @@ class ActionQA(Action):
                     message = f"We have the aircraft {vec[0]} for the flight between {fromloc.upper()} and {toloc.upper()}"
                 else:
                     for item in vec[:-1]:
-                        items = items + f" {item},"
+                        items = items + f" {meal},"
                     items = items[:-1] + f" and {vec[-1]}"
                     message = f"We have the aircraft {items} for the flights between {fromloc.upper()} and {toloc.upper()}"
             else:
