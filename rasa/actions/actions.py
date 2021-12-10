6d33011c7f57309a8d4bb9bb03ef2287d6d48a85
GabrielOlem <gme@cin.ufpe.br>
2021-12-10 17:41:08 -0300

    old models deleted

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -11,6 +11,8 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import UserUtteranceReverted
+from rasa_sdk.executor import CollectingDispatcher
 
 from datetime import datetime
 
